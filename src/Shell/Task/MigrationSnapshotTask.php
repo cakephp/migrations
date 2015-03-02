6f80eb3c328f4098c7454208741002c05d526a38
Walther Lalk <waltherlalk@gmail.com>
2015-03-02 09:56:29 +0200

    Revert back to BakeTemplate

--- src/Shell/Task/MigrationSnapshotTask.php
+++ src/Shell/Task/MigrationSnapshotTask.php
@@ -79,7 +79,7 @@ class MigrationSnapshotTask extends SimpleMigrationTask
             'collection' => $collection,
             'tables' => $tables,
             'action' => 'create_table',
-            'name' => $this->Template->viewVars['name'],
+            'name' => $this->BakeTemplate->viewVars['name'],
         ];
     }
 
