84416fa7b9ec58b08f01ad9351ad7a4fdbd75bfb
Yves P <havokinspiration@gmail.com>
2015-08-12 22:23:56 +0200

    Implements our own AbstractMigration class
    This allows us to prevent phinx from automatically adding a id column using a new `autoId` property. When set to false, phinx will not create a `id` column.
    This also add :
    - a new addPrimaryKeys method to specify which columns are PK
    - a new option "autoIncrement" to the addColumn method
    This is particularly useful if uuid columns are used

---
+++ src/Table.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Migrations;
+
+use Phinx\Db\Table as BaseTable;
+
+class Table extends BaseTable
+{
+
+    protected $primaryKeys = [];
+
+    /**
+     * Add a primary key to a database table.
+     *
+     * @param string|array $columns Table Column(s)
+     * @return Table
+     */
+    public function addPrimaryKey($columns)
+    {
+        $this->primaryKeys = $columns;
+        return $this;
+    }
+
+    /**
+     * You can pass `autoIncrement` as an option and it will be converted
+     * to the correct option for phinx to create the column with an
+     * auto increment attribute
+     *
+     * {@inheritdoc}
+     */
+    public function addColumn($columnName, $type = null, $options = array())
+    {
+        if (isset($options['autoIncrement']) && $options['autoIncrement'] === true) {
+            $options['identity'] = true;
+            unset($options['autoIncrement']);
+        }
+
+        return parent::addColumn($columnName, $type, $options);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create()
+    {
+        if ($this->options['id'] === false && !empty($this->primaryKeys)) {
+            $this->options['primary_key'] = $this->primaryKeys;
+        }
+        parent::create();
+    }
+}
