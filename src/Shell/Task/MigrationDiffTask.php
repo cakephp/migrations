919973b991f21047ce7eea234f269b5d5cc57aba
Yves P <havokinspiration@gmail.com>
2016-01-18 22:43:08 +0100

    If there are no migration files and an empty phinx log history, it should simply bake a snapshot

--- src/Shell/Task/MigrationDiffTask.php
+++ src/Shell/Task/MigrationDiffTask.php
@@ -27,16 +27,75 @@ class MigrationDiffTask extends SimpleMigrationTask
     use UtilTrait;
 
     /**
+     * Array of migrations that have already been migrated
+     *
+     * @var array
+     */
+    protected $migratedItems = [];
+
+    /**
+     * Path to the migration files
+     *
+     * @var string
+     */
+    protected $migrationsPath;
+
+    /**
+     * Migration files that are stored in the self::migrationsPath
+     *
+     * @var array
+     */
+    protected $migrationsFiles = [];
+
+    /**
+     * Name of the phinx log table
+     *
+     * @var string
+     */
+    protected $phinxTable;
+
+    /**
      * {@inheritDoc}
      */
     public function bake($name)
     {
-        $isSynced = $this->_checkSync();
+        $this->setup();
 
-        if (!$isSynced) {
+        if (!$this->checkSync()) {
             $this->error('Your migrations history is not in sync with your migrations files. ' .
                 'Make sure all your migrations have been migrated before baking a diff.');
         }
+
+        if (empty($this->migrationsFiles) && empty($this->migratedItems)) {
+            return $this->bakeSnapshot($name);
+        }
+    }
+
+    /**
+     * Sets up everything the baking process needs
+     *
+     * @return void
+     */
+    public function setup()
+    {
+        $this->migrationsPath = $this->getPath();
+        $this->migrationsFiles = glob($this->migrationsPath . '*.php');
+        $this->phinxTable = $this->getPhinxTable($this->plugin);
+
+        $connection = ConnectionManager::get($this->connection);
+        $tableExists = in_array($this->phinxTable, $connection->schemaCollection()->listTables());
+
+        $migratedItems = [];
+        if ($tableExists) {
+            $query = $connection->newQuery();
+            $migratedItems = $query
+                ->select(['version'])
+                ->from($this->phinxTable)
+                ->order(['version DESC'])
+                ->execute()->fetchAll('assoc');
+        }
+
+        $this->migratedItems = $migratedItems;
     }
 
     /**
@@ -44,38 +103,50 @@ class MigrationDiffTask extends SimpleMigrationTask
      *
      * @return bool Whether migrations history is sync or not
      */
-    protected function _checkSync()
+    protected function checkSync()
     {
-        $migrationsPath = $this->getPath();
-        $migrations = glob($migrationsPath . '*.php');
-        $tableName = $this->getPhinxTable($this->plugin);
-
-        $connection = ConnectionManager::get($this->connection);
-        $tableExists = in_array($tableName, $connection->schemaCollection()->listTables());
-
-        $migratedItems = [];
-        if ($tableExists) {
-            $query = $connection->newQuery();
-            $migratedItems = $query
-                ->select(['version'])
-                ->from($tableName)
-                ->order(['version DESC'])
-                ->limit(1)
-                ->execute()->fetchAll('assoc');
-        }
-
-        if (empty($migrations) && empty($migratedItems)) {
+        if (empty($this->migrationsFiles) && empty($this->migratedItems)) {
             return true;
         }
 
-        if (!empty($migratedItems)) {
-            $lastVersion = $migratedItems[0]['version'];
-            $lastFile = end($migrations);
+        if (!empty($this->migratedItems)) {
+            $lastVersion = $this->migratedItems[0]['version'];
+            $lastFile = end($this->migrationsFiles);
 
             return (bool)strpos($lastFile, $lastVersion);
         }
 
         return false;
+    }
+
+    /**
+     * Fallback method called to bake a snapshot when the phinx log history is empty and
+     * there are no migration files.
+     *
+     * @return int Value of the snapshot baking dispatch process
+     */
+    protected function bakeSnapshot($name)
+    {
+        $this->out('Your migrations history is empty and you do not have any migrations files.');
+        $this->out('Falling back to baking a snapshot...');
+        $dispatchCommand = 'bake migration_snapshot ' . $name;
+
+        if (!empty($this->params['connection'])) {
+            $dispatchCommand .= ' -c ' . $this->params['connection'];
+        }
+        if (!empty($this->params['plugin'])) {
+            $dispatchCommand .= ' -p ' . $this->params['plugin'];
+        }
+
+        $dispatch = $this->dispatchShell([
+            'command' => $dispatchCommand
+        ]);
+
+        if ($dispatch === 1) {
+            $this->error('Something went wrong during the snapshot baking. Please try again.');
+        }
+
+        return $dispatch;
     }
 
     /**
