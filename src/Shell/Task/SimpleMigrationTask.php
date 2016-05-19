d3e41c5952340dc1c6538cc2a5e9e69144041b89
alysson <alysson@lac.eti.br>
2016-05-18 21:36:43 -0300

    Set argument name as required

--- src/Shell/Task/SimpleMigrationTask.php
+++ src/Shell/Task/SimpleMigrationTask.php
@@ -116,7 +116,8 @@ abstract class SimpleMigrationTask extends SimpleBakeTask
         $parser->description(
             'Bake migration class.'
         )->addArgument('name', [
-            'help' => 'Name of the migration to bake. Can use Plugin.name to bake migration files into plugins.'
+            'help' => 'Name of the migration to bake. Can use Plugin.name to bake migration files into plugins.',
+            'required' => true
         ])->addOption('plugin', [
             'short' => 'p',
             'help' => 'Plugin to bake into.'
