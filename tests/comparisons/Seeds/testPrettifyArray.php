0f2953fb9cf621d1839f3bdbd9d4cc2d045ab822
Andrej Griniuk <andrej.griniuk@gmail.com>
2016-09-05 15:01:10 +0700

    fix typo

--- tests/comparisons/Seeds/testPrettifyArray.php
+++ tests/comparisons/Seeds/testPrettifyArray.php
@@ -11,7 +11,7 @@ class TextsSeed extends AbstractSeed
      *
      * Write your database seeder using this method.
      *
-     * More information on writing seeders is available here:
+     * More information on writing seeds is available here:
      * http://docs.phinx.org/en/latest/seeding.html
      *
      * @return void
