3ac2c99b81d8549c8e5f9c246e3bd389da0c3e0f
Peter 'Eax' Ahlers <eax@eax.dk>
2019-03-27 14:53:04 +0100

    Enable dry-run for migrations

--- src/Shell/MigrationsShell.php
+++ src/Shell/MigrationsShell.php
@@ -73,7 +73,8 @@ class MigrationsShell extends Shell
             ->addOption('no-interaction', ['short' => 'n'])
             ->addOption('template', ['short' => 't'])
             ->addOption('format', ['short' => 'f'])
-            ->addOption('only', ['short' => 'o'])
+	    ->addOption('only', ['short' => 'o'])
+	    ->addOption('dry-run', ['short' => 'x'])
             ->addOption('exclude', ['short' => 'e']);
     }
 
