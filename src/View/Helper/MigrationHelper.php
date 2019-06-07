403cd64a0a5cb87f3d09478f8f68670e9b191473
Marcelo Rocha <contato@omarcelo.com.br>
2019-06-06 23:11:45 -0300

    Added methods to use in create tables element

--- src/View/Helper/MigrationHelper.php
+++ src/View/Helper/MigrationHelper.php
@@ -550,4 +550,57 @@ class MigrationHelper extends Helper
         return Hash::extract($list, $path);
     }
 
+    /**
+     * Get data to use in create tables element
+     *
+     * @param string $table
+     * @return array
+     */
+    public function getCreateTableData($table)
+    {
+        $constraints = $this->constraints($table);
+        $indexes = $this->indexes($table);
+        $foreignKeys = [];
+        foreach ($constraints as $constraint) {
+            if ($constraint['type'] == 'foreign') {
+                $foreignKeys[] = $constraint['columns'];
+            }
+        }
+        $indexes = array_filter($indexes, function($index) use ($foreignKeys) {
+            return !in_array($index['columns'], $foreignKeys);
+        });
+        $result = compact('constraints', 'constraints', 'indexes', 'foreignKeys');
+
+        return $result;
+    }
+
+    /**
+     * Get data to use inside the create-tables element
+     *
+     * @param array $tables
+     * @return array
+     */
+    public function getCreateTablesElementData($tables)
+    {
+        $result = [
+            'constraints' => [],
+            'tables' => []
+        ];
+        foreach ($tables as $table) {
+            $data = $this->getCreateTableData($table);
+            $tableConstraintsNoUnique = array_filter(
+                $data['constraints'],
+                function($constraint) {
+                    return $constraint['type'] != 'unique';
+                }
+            );
+            if($tableConstraintsNoUnique) {
+                $result['constraints'][$table] =  $data['constraints'];
+            }
+            $result['tables'][$table] = $data;
+        }
+
+        return $result;
+    }
+
 }
