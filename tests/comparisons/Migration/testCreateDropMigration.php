7a8fb6c8d9aad4d2a2083f01b48563ae8e3159fc
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2022-05-18 17:46:23 +0200

    bake a valid drop migration

---
+++ tests/comparisons/Migration/testCreateDropMigration.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class DropUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->drop()->save();
+    }
+}
