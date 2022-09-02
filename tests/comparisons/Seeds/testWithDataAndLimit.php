e91de957da198155f55429a10c0f8c765d054513
Alejandro Ibarra <ajibarra@gmail.com>
2022-09-02 13:54:56 +0200

    Fix unit tests

--- tests/comparisons/Seeds/testWithDataAndLimit.php
+++ tests/comparisons/Seeds/testWithDataAndLimit.php
@@ -22,13 +22,13 @@ class EventsSeed extends AbstractSeed
     {
         $data = [
             [
-                'id' => 1,
+                'id' => '1',
                 'title' => 'Lorem ipsum dolor sit amet',
                 'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat.',
                 'published' => 'Y',
             ],
             [
-                'id' => 2,
+                'id' => '2',
                 'title' => 'Second event',
                 'description' => 'Second event description.',
                 'published' => 'Y',
