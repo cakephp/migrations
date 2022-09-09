a9926085a2371b407a016f028f46e7d9f9124350
Alejandro Ibarra <ajibarra@gmail.com>
2022-09-09 09:40:15 +0200

    Add new seed tests files for php 8.1

---
+++ tests/comparisons/Seeds/php81/testWithDataAndLimit.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+
+/**
+ * Events seed.
+ */
+class EventsSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor sit amet',
+                'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat.',
+                'published' => 'Y',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Second event',
+                'description' => 'Second event description.',
+                'published' => 'Y',
+            ],
+        ];
+
+        $table = $this->table('events');
+        $table->insert($data)->save();
+    }
+}
