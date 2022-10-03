792cc076556884acf33098f36e881540db612704
Arhell <arhell333@gmail.com>
2022-10-04 00:24:59 +0300

    update src folder links

--- src/Command/BakeMigrationCommand.php
+++ src/Command/BakeMigrationCommand.php
@@ -2,16 +2,16 @@
 declare(strict_types=1);
 
 /**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  *
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @license       https://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace Migrations\Command;
 
@@ -126,7 +126,7 @@ class BakeMigrationCommand extends BakeSimpleMigrationCommand
     {
         $parser = parent::getOptionParser();
         $text = <<<'TEXT'
-Create a blank or generated migration. Using the name of the migration 
+Create a blank or generated migration. Using the name of the migration
 Operations and table names will be inferred.
 
 <info>Examples</info>
@@ -188,7 +188,7 @@ Create a migration that adds (<warning>name VARCHAR(128)</warning>) to the <warn
 table.
 
 <warning>bin/cake bake migration AddSlugToProjects name:string[128]:unique</warning>
-Create a migration that adds (<warning>name VARCHAR(128)</warning> and a <warning>UNIQUE<.warning index) 
+Create a migration that adds (<warning>name VARCHAR(128)</warning> and a <warning>UNIQUE<.warning index)
 to the <warning>projects</warning> table.
 
 TEXT;
