d62b738b27355fd115e7489b3769c63c1363f174
Corey Taylor <corey.taylor.fl@gmail.com>
2023-09-14 13:00:33 -0500

    Add missing test properties

--- tests/TestCase/TestSuite/MigratorTest.php
+++ tests/TestCase/TestSuite/MigratorTest.php
@@ -23,6 +23,11 @@ use RuntimeException;
 
 class MigratorTest extends TestCase
 {
+    /**
+     * @var string
+     */
+    protected $restore;
+
     public function setUp(): void
     {
         parent::setUp();
