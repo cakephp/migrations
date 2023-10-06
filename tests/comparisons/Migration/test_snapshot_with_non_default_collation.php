f52981e6330477b89f4955e09448ab0edb6d440e
rrd108 <rrd@webmania.cc>
2023-10-06 13:34:22 +0200

    use snapshot file

---
+++ tests/comparisons/Migration/test_snapshot_with_non_default_collation.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class TestSnapshotWithNonDefaultCollation extends AbstractMigration
+{
+    /**
+     * Up Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-up-method
+     * @return void
+     */
+    public function up(): void
+    {
+        $this->table('articles')
+            ->addColumn('title', 'string', [
+                'collation' => 'utf8mb3_hungarian_ci',
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('category_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('product_id', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('note', 'string', [
+                'default' => '7.4',
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('counter', 'integer', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('active', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'category_id',
+                ],
+                [
+                    'name' => 'articles_category_fk',
+                ]
+            )
+            ->addIndex(
+                [
+                    'title',
+                ],
+                [
+                    'name' => 'articles_title_idx',
+                ]
+            )
+            ->create();
+
+        $this->table('articles')
+            ->addForeignKey(
+                'category_id',
+                'categories',
+                'id',
+                [
+                    'update' => 'NO_ACTION',
+                    'delete' => 'NO_ACTION',
+                    'constraint' => 'articles_category_fk'
+                ]
+            )
+            ->update();
+    }
+
+    /**
+     * Down Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-down-method
+     * @return void
+     */
+    public function down(): void
+    {
+        $this->table('articles')
+            ->dropForeignKey(
+                'category_id'
+            )->save();
+
+        $this->table('articles')->drop()->save();
+    }
+}
