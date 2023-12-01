b633cd70778bfb9326ff23d91491b4f1bc6ad3a5
Mark Story <mark@mark-story.com>
2023-12-01 00:06:19 -0500

    Fix phpcs

--- tests/TestCase/Db/Table/ForeignKeyTest.php
+++ tests/TestCase/Db/Table/ForeignKeyTest.php
@@ -3,8 +3,8 @@ declare(strict_types=1);
 
 namespace Migrations\Test\TestCase\Phinx\Db\Table;
 
-use Migrations\Db\Table\ForeignKey;
 use InvalidArgumentException;
+use Migrations\Db\Table\ForeignKey;
 use PHPUnit\Framework\TestCase;
 use RuntimeException;
 
