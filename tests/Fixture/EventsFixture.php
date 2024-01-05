c5f6e556f6a90c13a0d8b411bc7f478044002a4b
Mark Story <mark@mark-story.com>
2024-01-05 15:44:25 -0500

    Remove id values so that sqlserver stops complaining
    This could break other driver tests though.

--- tests/Fixture/EventsFixture.php
+++ tests/Fixture/EventsFixture.php
@@ -13,19 +13,16 @@ class EventsFixture extends TestFixture
      */
     public array $records = [
         [
-            'id' => 1,
             'title' => 'Lorem ipsum dolor sit amet',
             'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat.',
             'published' => 'Y',
         ],
         [
-            'id' => 2,
             'title' => 'Second event',
             'description' => 'Second event description.',
             'published' => 'Y',
         ],
         [
-            'id' => 3,
             'title' => 'Lorem ipsum dolor sit amet',
             'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat.
 Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget \'sollicitudin\' venenatis cum
