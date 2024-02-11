8254c7104dd0541ffb4be371eed2feeb3a6e980a
Mark Story <mark@mark-story.com>
2024-02-10 23:35:17 -0500

    Expand tests.

--- tests/TestCase/Command/StatusCommandTest.php
+++ tests/TestCase/Command/StatusCommandTest.php
@@ -5,6 +5,7 @@ namespace Migrations\Test\TestCase\Command;
 
 use Cake\Console\TestSuite\ConsoleIntegrationTestTrait;
 use Cake\Core\Configure;
+use Cake\Core\Exception\MissingPluginException;
 use Cake\TestSuite\TestCase;
 
 class StatusCommandTest extends TestCase
@@ -37,21 +38,36 @@ class StatusCommandTest extends TestCase
 
     public function testExecuteSimpleJson(): void
     {
-        $this->markTestIncomplete();
+        $this->exec('migrations status -c test --format json');
+        $this->assertExitSuccess();
+
+        assert(isset($this->_out));
+        $output = $this->_out->messages();
+        $parsed = json_decode($output[0], true);
+        $this->assertTrue(is_array($parsed));
+        $this->assertCount(1, $parsed);
+        $this->assertArrayHasKey('id', $parsed[0]);
+        $this->assertArrayHasKey('status', $parsed[0]);
+        $this->assertArrayHasKey('name', $parsed[0]);
     }
 
     public function testExecutePlugin(): void
     {
-        $this->markTestIncomplete();
+        $this->loadPlugins(['Migrator']);
+        $this->exec('migrations status -c test -p Migrator');
+        $this->assertExitSuccess();
+        $this->assertOutputRegExp("/\|.*?down.*\|.*?Migrator.*?\|/");
     }
 
     public function testExecutePluginDoesNotExist(): void
     {
-        $this->markTestIncomplete();
+        $this->expectException(MissingPluginException::class);
+        $this->exec('migrations status -c test -p LolNope');
     }
 
     public function testExecuteConnectionDoesNotExist(): void
     {
-        $this->markTestIncomplete();
+        $this->exec('migrations status -c lolnope');
+        $this->assertExitError();
     }
 }
