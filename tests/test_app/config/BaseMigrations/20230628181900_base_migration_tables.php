4aa08f2919609927b251af66b3d4daa3dff10977
Mark Story <mark@mark-story.com>
2024-10-25 23:35:45 -0400

    Consolidate test stubs
    one migration should be enough to do the integration testing I want

--- tests/test_app/config/BaseMigrations/20230628181900_base_migration_tables.php
+++ tests/test_app/config/BaseMigrations/20230628181900_base_migration_tables.php
@@ -2,11 +2,11 @@
 
 use Migrations\BaseMigration;
 
-class BaseCreateStoresTable extends BaseMigration
+class BaseMigrationTables extends BaseMigration
 {
     public function change(): void
     {
-        $table = $this->table('stores', ['collation' => 'utf8_bin']);
+        $table = $this->table('base_stores', ['collation' => 'utf8_bin']);
         $table
             ->addColumn('name', 'string')
             ->addTimestamps()
