ab830758589e2ca62597bf78af53d1d762d2b0ce
Mark Story <mark@mark-story.com>
2024-10-31 10:31:04 -0400

    Update more snapshots

--- tests/comparisons/Migration/sqlite/test_snapshot_auto_id_disabled_sqlite.php
+++ tests/comparisons/Migration/sqlite/test_snapshot_auto_id_disabled_sqlite.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use Migrations\AbstractMigration;
+use Migrations\BaseMigration;
 
-class TestSnapshotAutoIdDisabledSqlite extends AbstractMigration
+class TestSnapshotAutoIdDisabledSqlite extends BaseMigration
 {
     public bool $autoId = false;
 
--- tests/comparisons/Migration/sqlite/test_snapshot_plugin_blog_sqlite.php
+++ tests/comparisons/Migration/sqlite/test_snapshot_plugin_blog_sqlite.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use Migrations\AbstractMigration;
+use Migrations\BaseMigration;
 
-class TestSnapshotPluginBlogSqlite extends AbstractMigration
+class TestSnapshotPluginBlogSqlite extends BaseMigration
 {
     /**
      * Up Method.
