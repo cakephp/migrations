890f157dc145d00abf0135891154239ad406e053
Mark Story <mark@mark-story.com>
2024-11-04 23:51:05 -0500

    Update docs link in generated code

--- tests/comparisons/Migration/testCreateFieldLength.php
+++ tests/comparisons/Migration/testCreateFieldLength.php
@@ -9,7 +9,7 @@ class CreateUsers extends BaseMigration
      * Change Method.
      *
      * More information on this method is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
      * @return void
      */
     public function change(): void
--- tests/comparisons/Migration/testCreatePrimaryKeyUuid.php
+++ tests/comparisons/Migration/testCreatePrimaryKeyUuid.php
@@ -11,7 +11,7 @@ class CreateUsers extends BaseMigration
      * Change Method.
      *
      * More information on this method is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
      * @return void
      */
     public function change(): void
