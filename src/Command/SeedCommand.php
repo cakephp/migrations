1974eadd0a032ca3b8fabebd1d7bdfd158df1051
Jamison Bryant <jbryant@ticketsauce.com>
2025-07-21 14:58:51 -0400

    Remove duplicate warning usage

--- src/Command/SeedCommand.php
+++ src/Command/SeedCommand.php
@@ -128,7 +128,7 @@ class SeedCommand extends Command
         $versionOrder = $config->getVersionOrder();
 
         if ($config->isDryRun()) {
-            $io->warning('<warning>dry-run mode enabled</warning>');
+            $io->warning('dry-run mode enabled');
         }
         $io->verbose('<info>using connection</info> ' . (string)$args->getOption('connection'));
         $io->verbose('<info>using paths</info> ' . $config->getMigrationPath());
