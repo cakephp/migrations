8b10c470462612667e7aed71e2b9c1092948fd3e
Mark Story <mark@mark-story.com>
2025-09-08 11:57:17 -0400

    Fix deprecation warnings from plugin classes

---
+++ tests/test_app/Plugin/Blog/src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Blog;
+
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     */
+    protected ?string $name = 'Blog';
+}
---
+++ tests/test_app/Plugin/Migrator/src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Migrator;
+
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     */
+    protected ?string $name = 'Migrator';
+}
