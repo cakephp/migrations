b1da1a5c49718f7fc993bc062d18ac63d1d1ec85
Mark Story <mark@mark-story.com>
2025-09-17 22:51:46 -0400

    Fix tests
    - Column names are case-sensitive
    - Required fixes in cakephp/database to implement included columns

--- tests/TestCase/Db/Adapter/SqlserverAdapterTest.php
+++ tests/TestCase/Db/Adapter/SqlserverAdapterTest.php
@@ -229,7 +229,7 @@ class SqlserverAdapterTest extends TestCase
             ->addColumn('tag_id', 'integer', ['null' => false])
             ->save();
         $this->assertTrue($this->adapter->hasIndex('table1', ['user_id', 'tag_id']));
-        $this->assertTrue($this->adapter->hasIndex('table1', ['tag_id', 'USER_ID']));
+        $this->assertFalse($this->adapter->hasIndex('table1', ['tag_id', 'USER_ID']));
         $this->assertFalse($this->adapter->hasIndex('table1', ['tag_id', 'user_email']));
     }
 
@@ -717,10 +717,10 @@ class SqlserverAdapterTest extends TestCase
             ->addColumn('firstname', 'string')
             ->addColumn('lastname', 'string')
             ->save();
-        $this->assertFalse($table->hasIndex('email'));
+        $this->assertFalse($table->hasIndex(['email']));
         $table->addIndex(['email'], ['include' => ['firstname', 'lastname']])
             ->save();
-        $this->assertTrue($table->hasIndex('email'));
+        $this->assertTrue($table->hasIndex(['email']));
         $rows = $this->adapter->fetchAll("SELECT ic.is_included_column AS included
                         FROM   sys.indexes AS i
                         INNER JOIN sys.index_columns AS ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
