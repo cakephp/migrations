f7eab035a54272f8f7648c79f3887349ae7afcee
Mark Story <mark@mark-story.com>
2025-10-17 14:09:50 -0400

    Windows

--- src/BaseSeed.php
+++ src/BaseSeed.php
@@ -131,7 +131,7 @@ class BaseSeed implements SeedInterface
     {
         $name = static::class;
         if (str_starts_with($name, 'Migrations\BaseSeed@anonymous')) {
-            [, $path] = explode('/', $name, 2);
+            [, $path] = explode(DIRECTORY_SEPARATOR, $name, 2);
             [$path, ] = explode(':', $path, 2);
             $name = basename($path, '.php');
         }
