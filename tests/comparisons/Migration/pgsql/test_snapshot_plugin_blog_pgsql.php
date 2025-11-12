5331bac19e6e51a067378dbbff7c3c927429296d
mscherer <dereuromark@web.de>
2025-11-12 13:37:39 +0100

    Fixes.

--- tests/comparisons/Migration/pgsql/test_snapshot_plugin_blog_pgsql.php
+++ tests/comparisons/Migration/pgsql/test_snapshot_plugin_blog_pgsql.php
@@ -104,6 +104,57 @@ class TestSnapshotPluginBlogPgsql extends BaseMigration
             )
             ->create();
 
+        $this->table('composite_pks', ['id' => false, 'primary_key' => ['id', 'name']])
+            ->addColumn('id', 'uuid', [
+                'default' => 'a4950df3-515f-474c-be4c-6a027c1957e7',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => '',
+                'limit' => 10,
+                'null' => false,
+            ])
+            ->create();
+
+        $this->table('events')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('published', 'string', [
+                'default' => 'N',
+                'limit' => 1,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('orders')
+            ->addColumn('product_category', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+            ])
+            ->addColumn('product_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+            ])
+            ->addIndex(
+                $this->index([
+                        'product_category',
+                        'product_id',
+                    ])
+                    ->setName('orders_product_category_idx')
+            )
+            ->create();
+
         $this->table('parts')
             ->addColumn('name', 'string', [
                 'default' => null,
@@ -117,6 +168,143 @@ class TestSnapshotPluginBlogPgsql extends BaseMigration
             ])
             ->create();
 
+        $this->table('products')
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('slug', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => true,
+            ])
+            ->addColumn('category_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => true,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'precision' => 6,
+                'scale' => 6,
+            ])
+            ->addColumn('modified', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'precision' => 6,
+                'scale' => 6,
+            ])
+            ->addIndex(
+                $this->index([
+                        'id',
+                        'category_id',
+                    ])
+                    ->setName('products_category_unique')
+                    ->setType('unique')
+            )
+            ->addIndex(
+                $this->index('slug')
+                    ->setName('products_slug_unique')
+                    ->setType('unique')
+            )
+            ->addIndex(
+                $this->index('title')
+                    ->setName('products_title_idx')
+            )
+            ->create();
+
+        $this->table('special_pks', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'uuid', [
+                'default' => 'a4950df3-515f-474c-be4c-6a027c1957e7',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 256,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('special_tags')
+            ->addColumn('article_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+            ])
+            ->addColumn('author_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => true,
+            ])
+            ->addColumn('tag_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+            ])
+            ->addColumn('highlighted', 'boolean', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('highlighted_time', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'precision' => 6,
+                'scale' => 6,
+            ])
+            ->addIndex(
+                $this->index('article_id')
+                    ->setName('special_tags_article_unique')
+                    ->setType('unique')
+            )
+            ->create();
+
+        $this->table('texts', ['id' => false])
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('description', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+
+        $this->table('users')
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 256,
+                'null' => true,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 256,
+                'null' => true,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'precision' => 6,
+                'scale' => 6,
+            ])
+            ->addColumn('updated', 'timestamp', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+                'precision' => 6,
+                'scale' => 6,
+            ])
+            ->create();
+
         $this->table('articles')
             ->addForeignKey(
                 $this->foreignKey('category_id')
@@ -125,6 +313,34 @@ class TestSnapshotPluginBlogPgsql extends BaseMigration
                     ->setOnDelete('NO_ACTION')
                     ->setOnUpdate('NO_ACTION')
                     ->setName('articles_category_fk')
+            )
+            ->update();
+
+        $this->table('orders')
+            ->addForeignKey(
+                $this->foreignKey([
+                        'product_category',
+                        'product_id',
+                    ])
+                    ->setReferencedTable('products')
+                    ->setReferencedColumns([
+                        'category_id',
+                        'id',
+                    ])
+                    ->setOnDelete('CASCADE')
+                    ->setOnUpdate('CASCADE')
+                    ->setName('orders_product_fk')
+            )
+            ->update();
+
+        $this->table('products')
+            ->addForeignKey(
+                $this->foreignKey('category_id')
+                    ->setReferencedTable('categories')
+                    ->setReferencedColumns('id')
+                    ->setOnDelete('CASCADE')
+                    ->setOnUpdate('CASCADE')
+                    ->setName('products_category_fk')
             )
             ->update();
     }
@@ -144,8 +360,29 @@ class TestSnapshotPluginBlogPgsql extends BaseMigration
                 'category_id'
             )->save();
 
+        $this->table('orders')
+            ->dropForeignKey(
+                [
+                    'product_category',
+                    'product_id',
+                ]
+            )->save();
+
+        $this->table('products')
+            ->dropForeignKey(
+                'category_id'
+            )->save();
+
         $this->table('articles')->drop()->save();
         $this->table('categories')->drop()->save();
+        $this->table('composite_pks')->drop()->save();
+        $this->table('events')->drop()->save();
+        $this->table('orders')->drop()->save();
         $this->table('parts')->drop()->save();
+        $this->table('products')->drop()->save();
+        $this->table('special_pks')->drop()->save();
+        $this->table('special_tags')->drop()->save();
+        $this->table('texts')->drop()->save();
+        $this->table('users')->drop()->save();
     }
 }
