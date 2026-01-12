5882a530b54a8741f9b3d87a91d93dfe1b8cb02c
Mark Story <mark@mark-story.com>
2026-01-11 19:33:25 -0500

    Update tests/TestCase/Command/EntryCommandTest.php

--- tests/TestCase/Command/EntryCommandTest.php
+++ tests/TestCase/Command/EntryCommandTest.php
@@ -45,7 +45,7 @@ class EntryCommandTest extends TestCase
 
         $this->assertExitSuccess();
         $this->assertOutputContains('Using <info>builtin</info> backend');
-        $this->assertOutputContains('migrations:');
+        $this->assertOutputContains('migrations</info>:');
         $this->assertOutputContains('migrations migrate');
         $this->assertOutputContains('migrations status');
         $this->assertOutputContains('migrations rollback');
