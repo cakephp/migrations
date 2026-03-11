e9045c9a4923eed9de61636415c338457cf6f976
Jamison Bryant <jamison@bryant.ai>
2026-03-11 15:51:36 -0400

    Consolidate null guard conditions in BakeMigrationDiffCommand

--- src/Command/BakeMigrationDiffCommand.php
+++ src/Command/BakeMigrationDiffCommand.php
@@ -279,11 +279,7 @@ class BakeMigrationDiffCommand extends BakeSimpleMigrationCommand
             // changes in columns meta-data
             foreach ($currentColumns as $columnName) {
                 $column = $this->safeGetColumn($currentSchema, $columnName);
-                if ($column === null) {
-                    continue;
-                }
-
-                if (!in_array($columnName, $oldColumns, true)) {
+                if ($column === null || !in_array($columnName, $oldColumns, true)) {
                     continue;
                 }
 
